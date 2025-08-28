dbae5fd5a20a003b2ceb44055c190799cef10d4d
Acudofy <Joaquimhekje@gmail.com>
2025-08-28 15:33:39 +0200

    Initial setup of player

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+class_name player
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	pass
