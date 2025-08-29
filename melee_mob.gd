e86056eba3211b2fb8172f43a1a1fffeb077fd93
Acudofy <Joaquimhekje@gmail.com>
2025-08-29 13:25:12 +0200

    Manual revert of previous push

--- melee_mob.gd
+++ melee_mob.gd
@@ -1,32 +1,35 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-@export var speed: float = 200.0
-@export var agent: NavigationAgent2D
-@export var playerchar: CharacterBody2D
-var path: PackedVector2Array = []
-var current_point_index: int = 0
-var speed_scalar: float = 1000.0
-func _ready() -> void:
-	agent.target_desired_distance = 10
-	
+var movement_speed: float = 200.0
+var movement_target_position: Vector2 = Vector2(60.0,180.0)
 
-func set_target(target_position: Vector2):
-	agent.target_position = target_position
-	agent.get_next_path_position()
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+
+func _ready():
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 4.0
+	navigation_agent.target_desired_distance = 4.0
+
+	# Make sure to not await during _ready.
+	actor_setup.call_deferred()
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(movement_target_position)
+
+func set_movement_target(movement_target: Vector2):
+	navigation_agent.target_position = movement_target
 
 func _physics_process(delta):
-	set_target(playerchar.global_position)
-	if not agent.is_target_reached():
-		var target_point: Vector2 = agent.get_next_path_position()
-		var dir: Vector2 = (target_point - global_position).normalized()
-		apply_central_force(dir*speed_scalar)
-		var distance = global_position.distance_to(target_point)
-		print(agent.distance_to_target())
-		#if distance < 10.0:
-			#current_point_index += 1
-		#else:
-			#var direction = (target_point - global_position).normalized()
-			#linear_velocity = direction * speed
-	#else:
-		#linear_velocity = Vector2.ZERO
-	
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	move_and_slide()
