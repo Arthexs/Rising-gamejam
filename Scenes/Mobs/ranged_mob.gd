8731807283377bb20770fc86c965cd578e09b41c
Acudofy <Joaquimhekje@gmail.com>
2025-08-29 16:41:19 +0200

    Restored health module and added ranged_mob functionality

--- Scenes/Mobs/ranged_mob.gd
+++ Scenes/Mobs/ranged_mob.gd
@@ -5,29 +5,86 @@ var movement_target_position: Vector2 = Vector2(60.0,180.0)
 
 @export var nav_agent: NavigationAgent2D
 @export var player: Player
-@export var acceleration: float = 10.0
-var movement_force = acceleration * mass
+@export var acceleration: float = 3.0
+@export var max_vel: float = 300.0
+@export var friction_coefficient: float = 20 # px/m/s
+var movement_force = acceleration * mass * 32 # px/m
+
+@export var impact_threshold: float = 0.6 # Fraction of velocity needed to be lost to be considered impacted
+@export var impact_damage_tuner: float = 1.0
+
+var flying: bool = false
+
+var prev_velocity: Vector2 = Vector2.ZERO
+
+var health_module: HealthModule
 
 func _ready():
 	# These values need to be adjusted for the actor's speed
 	# and the navigation layout.
+	health_module = get_node("HealthModule")
 	nav_agent.target_desired_distance = 10.0
+	contact_monitor = true
+	max_contacts_reported = 8
 
 func set_target(target: Vector2):
 	nav_agent.target_position = target
 	nav_agent.get_next_path_position()
 
 func _physics_process(delta):
+	var delta_vel: float = (linear_velocity - prev_velocity).length()
+
+	if delta_vel/prev_velocity.length() > impact_threshold && flying:
+		var force: float = delta_vel/delta * mass
+		var damage: float = force/100000
+		print("damage taken: ", damage)
+		health_module.take_damage(damage)
+		linear_velocity = Vector2.ZERO
+			#apply_impulse(applied_impulse * 1.0, state.get_contact_collider_position(i))
+			#collider.apply_impulse(-applied_impulse, state.get_contact_collider_position(i))
+			#print("apply impulse")
+
+	
 	set_target(player.global_position)
 	if not nav_agent.is_target_reached():
 		var direction: Vector2 = (nav_agent.get_next_path_position() - global_position).normalized()
-		print(player.global_position)
-		print(nav_agent.get_next_path_position())
-		print(global_position)
-		
 		apply_movement(direction)
+	
+	apply_friction()
+	prev_velocity = linear_velocity
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var applied_impulse: Vector2 = Vector2.ZERO
+	for i: int in range(get_contact_count()):
+		applied_impulse += state.get_contact_impulse(i)
+		#print(i)
+		var collider: Node2D = state.get_contact_collider_object(i) as Node2D
+		#var collider_rid: RID = state.get_contact_collider(i)
+		if collider is TileMapLayer:
+			print("collided with wall")
+		
+		if collider is Hammer:
+			pass
+			#print("collided with hammer")
+		
+		if collider.is_in_group("enemies"):
+			print("collided with enemy")
+		
+
+func apply_friction() -> void:
+	#print(linear_velocity.length(), "/", max_vel)
+	if linear_velocity.length() < max_vel:
+		flying = false
+		apply_central_force(-linear_velocity.normalized() * friction_coefficient)
+	else:
+		flying = true
+		apply_central_force(-linear_velocity.normalized() * friction_coefficient*0.1)
 
 func apply_movement(direction: Vector2) -> void:
+	if flying:
+		print("flying")
+		return
+	
 	if direction.dot(linear_velocity) < 0:
 		apply_central_force(direction * movement_force*1.5)
 	else:
--- Scenes/Player/health_module.gd
+++ Scenes/Player/health_module.gd
@@ -1,19 +1,36 @@
-extends Node2D
+extends ProgressBar
 
+class_name HealthModule
+
+@export var box: CollisionShape2D
 @export var max_health: float = 100.0
-var health: float = max_health
+@onready var health: float = max_health
 
 signal death()
 signal damaged(damage: float)
 
+func _ready() -> void:
+	scale_bar()
+
+func _process(delta: float) -> void:
+	value = health/max_health * max_value
+
 func take_damage(damage: float) -> void:
 	health -= damage
 	health = min(health, max_health)
-	
+		
 	damaged.emit(damage)
 	
 	if health <= 0.0:
 		die()
+
+func scale_bar() -> void:
+	var rect: Rect2 = box.shape.get_rect()
+	print(rect.position)
+	size.x = rect.size.x * 0.7
+	global_position = rect.position + box.global_position
+	global_position.y -= size.y + 8
+	global_position.x += rect.size.x/2 - size.x/2 
 
 func die() -> void:
 	death.emit()
